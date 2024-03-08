2a0447e273588a7e77c52443a6e7eb31308ef5d7
Pujan Adhikari <Puzanadhikary@gmail.com>
2024-03-08 14:44:43 +0545

    homepage navigation bar changes

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,18 +1,164 @@
+import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final _pageController = PageController(initialPage: 0);
+  final _controller = NotchBottomBarController(index: 0);
+  int maxCount = 5;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  final List<Widget> bottomBarPages = [
+    const Page1(),
+    const Page2(),
+    const Page3(),
+    const Page4(),
+    const Page5(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hello World"),
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: List.generate(
+            bottomBarPages.length, (index) => bottomBarPages[index]),
       ),
+      extendBody: true,
+      bottomNavigationBar: (bottomBarPages.length <= maxCount)
+          ? AnimatedNotchBottomBar(
+              notchBottomBarController: _controller,
+              color: Colors.white,
+              showLabel: true,
+              notchColor: Colors.black87,
+              removeMargins: false,
+              bottomBarWidth: 500,
+              durationInMilliSeconds: 300,
+              bottomBarItems: [
+                const BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.home_filled,
+                    color: Colors.blueGrey,
+                  ),
+                  activeItem: Icon(
+                    Icons.home_filled,
+                    color: Colors.lightGreen,
+                  ),
+                  itemLabel: 'Home',
+                ),
+                const BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.star,
+                    color: Colors.blueGrey,
+                  ),
+                  activeItem: Icon(
+                    Icons.star,
+                    color: Colors.lightGreen,
+                  ),
+                  itemLabel: 'Rate',
+                ),
+                BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.search,
+                    color: Colors.blueGrey,
+                  ),
+                  activeItem: Icon(
+                    Icons.search_outlined,
+                    color: Colors.lightGreen,
+                  ),
+                  itemLabel: 'Search',
+                ),
+                const BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.settings,
+                    color: Colors.blueGrey,
+                  ),
+                  activeItem: Icon(
+                    Icons.settings,
+                    color: Colors.lightGreen,
+                  ),
+                  itemLabel: 'Setting',
+                ),
+                const BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.person,
+                    color: Colors.blueGrey,
+                  ),
+                  activeItem: Icon(
+                    Icons.person,
+                    color: Colors.lightGreen,
+                  ),
+                  itemLabel: 'Profile',
+                ),
+              ],
+              onTap: (index) {
+                _pageController.jumpToPage(index);
+              },
+            )
+          : null,
     );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(child: Text('Page 1')));
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(child: Text('Page 2')));
+  }
+}
+
+class Page3 extends StatelessWidget {
+  const Page3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: const Center(child: Text('Page 3')));
+  }
+}
+
+class Page4 extends StatelessWidget {
+  const Page4({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(child: Text('Page 4')));
+  }
+}
+
+class Page5 extends StatelessWidget {
+  const Page5({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: const Center(child: Text('Page 5')));
   }
 }
