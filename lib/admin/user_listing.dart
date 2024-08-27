ed65941c593b9e0d914531f827fac57cf37fd86a
puzanadhikari <puzanadhikary@gmail.com>
2024-08-27 13:14:19 +0545

    chnages

--- lib/admin/user_listing.dart
+++ lib/admin/user_listing.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import '../Constants/styleConsts.dart';
+
 class UserListing extends StatefulWidget {
   const UserListing({super.key});
 
@@ -7,10 +11,165 @@ class UserListing extends StatefulWidget {
 }
 
 class _UserListingState extends State<UserListing> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 50.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(
+                  radius: 20,
+                  backgroundColor: kThemeColor,
+                  backgroundImage: const NetworkImage(
+                      "https://media.licdn.com/dms/image/D5603AQFD6ld3NWc2HQ/profile-displayphoto-shrink_200_200/0/1684164054868?e=2147483647&v=beta&t=cwQoyfhgAl_91URX5FTEXLwLDEHWe1H337EMebpgntQ"),
+                ),
+                Text("Users",
+                    style: TextStyle(
+                        color: kThemeColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25)),
+                IconButton(
+                  icon: Icon(
+                    Icons.notifications_none_outlined,
+                    color: kThemeColor,
+                    size: 30,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Divider(color: Color(0xFFD9D6D6), height: 2, thickness: 1),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _firestore.collection('users').snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                    return const Center(child: Text('No users found.'));
+                  }
 
+                  final users = snapshot.data!.docs;
+
+                  final adminUsers = users.where((user) => user['userType'] == 'Admin').toList();
+                  final sellerUsers = users.where((user) => user['userType'] == 'Seller').toList();
+                  final buyerUsers = users.where((user) => user['userType'] == 'Buyer').toList();
+
+                  return ListView(
+                    children: [
+                      _buildUserSection('Admin', adminUsers),
+                      const SizedBox(height: 16),
+                      _buildUserSection('Seller', sellerUsers),
+                      const SizedBox(height: 16),
+                      _buildUserSection('Buyer', buyerUsers),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserSection(String title, List<QueryDocumentSnapshot> users) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: kThemeColor,
+          ),
+        ),
+        const SizedBox(height: 8),
+        ...users.map((user) {
+          final email = user['email'] ?? 'No email';
+          final username = user['username'] ?? 'No username';
+          final photoUrl =  "https://media.licdn.com/dms/image/D5603AQFD6ld3NWc2HQ/profile-displayphoto-shrink_200_200/0/1684164054868?e=2147483647&v=beta&t=cwQoyfhgAl_91URX5FTEXLwLDEHWe1H337EMebpgntQ";
+
+          return Card(
+            margin: const EdgeInsets.symmetric(vertical: 8.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+            elevation: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12.0),
+                    child: Container(
+                      width: 80,
+                      height: 80,
+                      color: Colors.grey.shade200,
+                      child: photoUrl.isNotEmpty
+                          ? Image.network(
+                        photoUrl,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) => const Icon(Icons.image_not_supported, size: 80, color: Colors.grey),
+                      )
+                          : const Icon(Icons.image_not_supported, size: 80, color: Colors.grey),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          username.toUpperCase(),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          email,
+                          style: TextStyle(color: Colors.grey.shade500),
+                        ),
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: kThemeColor,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        Icons.wifi_calling_3,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }).toList(),
+      ],
     );
   }
 }
